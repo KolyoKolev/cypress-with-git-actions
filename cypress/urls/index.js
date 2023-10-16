8ccc8a0d6d974b6d8eac63894253b96be23f3e2a
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2023-10-16 13:10:53 +0300

    add new file

---
+++ cypress/urls/index.js
@@ -0,0 +1,8 @@
+export const URLS = {
+  baseUrl: 'https://practice.automationtesting.in/',
+  products: {
+    seleniumRuby:
+      'https://practice.automationtesting.in/product/selenium-ruby/',
+  },
+  myAccountPage: 'https://practice.automationtesting.in/my-account/',
+};
